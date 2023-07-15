bc7f5f8664af821d6d7c03c3e788ea5dc40d75dc
liyaowhen <alfundolst@gmail.com>
2023-07-15 16:05:45 -0700

    first commit

---
+++ src/MainView.hx
@@ -0,0 +1,13 @@
+package;
+
+import haxe.ui.components.Button;
+import haxe.ui.containers.VBox;
+import haxe.ui.events.MouseEvent;
+
+@:build(haxe.ui.ComponentBuilder.build("assets/main-view.xml"))
+class MainView extends VBox {
+    public function new() {
+        super();
+    }
+    
+}
